9f5f20dad4ddff8d88cf39c8714d4c0d6dbb95b6
Graeme Yeates <yeatesgraeme@gmail.com>
2015-07-22 12:47:42 -0400

    Fix requirements from pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+  requires = [x.strip() for x in f.readlines()]
+
 setup(
   name = 'cdn-it',
   packages = ['cdn_it'],
-  version = '1.0.7',
+  version = '1.0.8',
   description = 'Quickly create an auto updating jsDelivr project',
   author = 'Graeme Yeates',
   author_email = 'megawac@gmail.com',
   url = 'https://github.com/megawac/cdn-it',
-  download_url = 'https://github.com/megawac/cdn-it/tarball/v1.0.7',
+  download_url = 'https://github.com/megawac/cdn-it/tarball/v1.0.8',
   keywords = ['cdn', 'jsdelivr'], # arbitrary keywords
   classifiers = [],
+  install_requires = requires,
   entry_points={
     'console_scripts': [
         'cdn-it=cdn_it:main'
